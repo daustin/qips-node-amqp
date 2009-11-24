1ef7f7c45da3da981a9f7ee43dc5fe53528e5e9b
David Austin <daustin@MEATH.GCRC.UPENN.EDU>
2009-11-24 17:02:26 -0500

    Added some basic logic to shutdown a ruby process in the cron job

--- bin/status-updater.rb
+++ bin/status-updater.rb
@@ -13,8 +13,21 @@ require 'yaml'
 require 'json'
 require 'net/http'
 
+
 STATUS_FILE = './tmp/status.yml'
 STATUS_URL = 'http://localhost:3000/instance/set_status'
+NODE_DAEMON_PATH = '/Users/daustin/git_repos/qips-node-amqp/bin/qips-node-amqp'
+KILL_SIGNAL = 15
+
+# Some of the more commonly used signals:
+#     1       HUP (hang up)
+#     2       INT (interrupt)
+#     3       QUIT (quit)
+#     6       ABRT (abort)
+#     9       KILL (non-catchable, non-ignorable kill)
+#     14      ALRM (alarm clock)
+#     15      TERM (software termination signal)
+
 
 unless File.size?(STATUS_FILE) then
   puts "YAML file not found or empty: #{STATUS_FILE}"
@@ -31,12 +44,12 @@ yml_hash = YAML.load(yml_file)
 # then we get process CPU
 # for now lets just add a dummy CPU
 
-yml_hash['system_cpu_usage'] = 0.20
-yml_hash['ruby_cpu_usage'] = 0.30
+yml_hash['system_cpu_usage'] = 0.11
+yml_hash['ruby_cpu_usage'] = 0.22
 yml_hash['system_mem_usage'] = 1234
 yml_hash['ruby_mem_usage'] = 4321
-yml_hash['top_pid'] = 2273
-yml_hash['ruby_pid_status'] = "zombie"
+yml_hash['top_pid'] = 5678
+yml_hash['ruby_pid_status'] = "TEST"
 
 
 # then we encode it to json and pass it back to the server
@@ -58,7 +71,15 @@ rescue Exception => e
 end
 
 if kill
+  
+  #First kill process, then start another one. 
+  
   puts "KILL KILL KILL process: #{yml_hash['ruby_pid']}"
+  
+  Process.kill(KILL_SIGNAL, yml_hash['ruby_pid'].to_i)
+  
+  p1 = fork { system("ruby #{NODE_DAEMON_PATH}") }
+  Process.detach(p1)
   
 end
 
